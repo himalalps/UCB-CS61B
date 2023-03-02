f8744520b08b23f6746a7c6803dc3e7f20634d5e
himalalps <haoyu_t@mail.ustc.edu.cn>
2023-03-02 15:50:06 +0800

    hello world

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -2,4 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 	}
-} 
+}
+
+/**
+ * all code in Java must be part of a class
+ * delimit the beginning and end of segments of code with {}
+ * all statements in Java must end in a semi-colon
+ * need public static void main()
+ */
